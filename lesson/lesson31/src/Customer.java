a9d723a746fa7ade522157cf71e472a3c95fc4ea
fatimaguliyeva2007 <quliyevafatime2100@gmail.com>
2026-08-13 20:45:02 +0400

    Add balance field

--- lesson/lesson31/src/Customer.java
+++ lesson/lesson31/src/Customer.java
@@ -1,10 +1,12 @@
 public class Customer {
     private String name;
     private int age;
+    private double balance;
 
-    public Customer(String name, int age) {
+    public Customer(String name, int age, double balance) {
         this.name = name;
         this.age = age;
+        this.balance = balance;
     }
 
     public String getName() {
